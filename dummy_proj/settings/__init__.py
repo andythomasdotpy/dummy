200539701ce4868a9091b50501a76b39763d9b9b
Andy <andythomas.py@gmail.com>
2023-01-23 15:49:27 -0600

    Updated init.py to os.envviron to access AWS env vars

--- dummy_proj/settings/__init__.py
+++ dummy_proj/settings/__init__.py
@@ -1,9 +1,12 @@
+import os
 from decouple import config
 
 
 # settings_path = "prod"
 
-if config("settings_path") == "prod":
+# os.environ.get('TWITTER_ACCESS_TOKEN')
+
+if os.environ.get('SETTINGS_PATH') == "prod":
     from .prod import *
 else:
     from .dev import *
