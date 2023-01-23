1b8f91b1762395f821f4b5e4f2bcf3f7b6696615
Andy <andythomas.py@gmail.com>
2023-01-23 14:53:46 -0600

    Second commit

--- dummy_proj/settings.py
+++ dummy_proj/settings.py
@@ -26,10 +26,11 @@ SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    "127.0.0.1",
     "anpythomas.com",
     "www.anpythomas.com",
+    "dummy-env.eba-nwpabs5q.us-west-2.elasticbeanstalk.com",
 ]
-"dummy-env.eba-nwpabs5q.us-west-2.elasticbeanstalk.com"
 
 # Application definition
 
