76f11172e13c506e0f0f92031dfe200793684b92
Andy <andythomas.py@gmail.com>
2023-01-23 17:05:19 -0600

    Added try block to init.py. dev.py and prod.py paths both executing corrently

--- dummy_proj/settings/__init__.py
+++ dummy_proj/settings/__init__.py
@@ -5,13 +5,14 @@ from decouple import config
 # SETTINGS_PROD = "prod"
 
 # os.environ.get('TWITTER_ACCESS_TOKEN')
+try:
+    print(f"printing **before** {config('SETTINGS_PATH')}")
 
-print(f"printing **before** {config('SETTINGS_PATH')}")
-
-if config('SETTINGS_PATH') == "prod":
-    print("prod.py settings activated!")
-    from .prod import *
-else:
+    if config('SETTINGS_PATH') == "prod":
+        print("prod.py settings activated!")
+        from .prod import *
+except:
+    print("SETTINGS_PATH not equal to prod..")
     print("dev.py settings activated!")
     from .dev import *
 
