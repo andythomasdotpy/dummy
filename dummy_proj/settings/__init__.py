45fff1f2eb407512bbcf93b273969e1fc602c6ff
Andy <andythomas.py@gmail.com>
2023-01-23 15:26:16 -0600

    Declared settings_path variable in .env and in AWS Env

--- dummy_proj/settings/__init__.py
+++ dummy_proj/settings/__init__.py
@@ -1,1 +1,12 @@
-from .dev import *
+from decouple import config
+
+
+# settings_path = "prod"
+
+if config("settings_path") == "prod":
+    from .prod import *
+else:
+    from .dev import *
+
+
+
---
+++ dummy_proj/settings/prod.py
@@ -0,0 +1,14 @@
+from .base import *
+
+print("prod")
+
+SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k"
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "anpythomas.com",
+    "www.anpythomas.com",
+    "dummy-env.eba-nwpabs5q.us-west-2.elasticbeanstalk.com",
+]
