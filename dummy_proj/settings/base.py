24b21636061e8843707193b97c7113d694bb18e6
Andy <andythomas.py@gmail.com>
2023-01-23 15:10:59 -0600

    Settings.py two paths base.py and dev.py

--- dummy_proj/settings/base.py
+++ dummy_proj/settings/base.py
@@ -3,18 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "anpythomas.com",
-    "www.anpythomas.com",
-    "dummy-env.eba-nwpabs5q.us-west-2.elasticbeanstalk.com",
-]
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
---
+++ dummy_proj/settings/dev.py
@@ -0,0 +1,16 @@
+from decouple import config
+
+from .base import *
+
+print("dev")
+
+SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k"
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "anpythomas.com",
+    "www.anpythomas.com",
+    "dummy-env.eba-nwpabs5q.us-west-2.elasticbeanstalk.com",
+]
