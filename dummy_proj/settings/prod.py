d44bd1fcbbff8a2a8ee99558d0718f311c883a69
Andy <andythomas.py@gmail.com>
2023-01-23 20:29:09 -0600

    Add env vars to aws env

--- dummy_proj/settings/prod.py
+++ dummy_proj/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 print("prod")
 
-SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k"
+# SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k"
 
 DEBUG = False
 
