7e8edd6bb5fcc3a4fc693cea9873b307aae9a8c0
Andy <andythomas.py@gmail.com>
2023-01-23 20:33:01 -0600

    prod.py - add config var

--- dummy_proj/settings/prod.py
+++ dummy_proj/settings/prod.py
@@ -1,8 +1,10 @@
+from decouple import config
+
 from .base import *
 
 print("prod")
 
-# SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k"
+SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = False
 
