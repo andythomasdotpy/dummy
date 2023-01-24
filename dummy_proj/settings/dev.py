46fbc6b1de03731b6e863ea842e1ea3594fbfca9
Andy <andythomas.py@gmail.com>
2023-01-23 18:54:54 -0600

    Added HTTP and HSTS settings to prod.py

--- dummy_proj/settings/dev.py
+++ dummy_proj/settings/dev.py
@@ -4,7 +4,7 @@ print("dev")
 
 SECRET_KEY = "django-insecure-v#6tp%xx)vs*3iza9l0^8^13p-^lnf$*6f0sfpz6)vf*763!$k"
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
--- dummy_proj/settings/prod.py
+++ dummy_proj/settings/prod.py
@@ -10,3 +10,13 @@ ALLOWED_HOSTS = [
     "anpythomas.com",
     "www.anpythomas.com",
 ]
+
+# HTTPS settings
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+# HSTS settings
+SECURE_HSTS_SECONDS = 31536000 # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
